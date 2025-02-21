f16f6ec6dbe9e51b20044cf17dc792ada213fee8
UlyaFed <uliana.fedak@dci-student.org>
2025-02-21 13:29:34 +0100

    phone number

--- amazon_expenses_tracker.py
+++ amazon_expenses_tracker.py
@@ -3,7 +3,6 @@ This project would be designed to help users track their Amazon expenses. It wou
 
 """
 # Registration (accepting user name and valid password from the terminal)
-
 import re 
 
 def validate_password():
@@ -21,3 +20,19 @@ def validate_password():
 
 user_name = input("Create a name: ")
 validate_password()
+
+# Ask the user to iput his valid german mobile number.
+def phone_number():
+    pattern = "^(?:\+49[\s-]?|0049[\s-]?|0[\s-]?)[1-9]\d{1,4}(?:[\s-]?\d{2,10})+$"
+
+    
+    while True:
+        phone_number = input("Enter your German phone number: ").strip()
+        
+        if re.match(pattern,phone_number):
+            print("Thank you! Phone number is saved.")
+            break
+        else:
+            print("Invalid phone number. Try again (e.g., +4917612345678 or 017612345678): ")
+            
+phone_number()
